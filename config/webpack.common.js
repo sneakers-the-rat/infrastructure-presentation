7e720b5e5842766a2764e86d4306889d6dada0e9
sneakers-the-rat <JLSaunders987@gmail.com>
2021-04-23 22:10:04 -0700

    publicpath for gh pages

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
   output: {
     path: path.resolve( __dirname, '../dist' ),
     filename: '[name].bundle.js',
-    publicPath: '/infrastructure',
+    publicPath: '/infrastructure-presentation',
   },
   // devtool: 'source-map',
   module: {
